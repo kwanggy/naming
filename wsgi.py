fd58b6edc16605fe6974dbc09c2de1bb174fbb5a
Chihyun Lim <chidoli37@gmail.com>
2014-01-04 04:30:32 -0500

    prod environ port

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 if __name__ == '__main__':
-    from app.config import conf
+    from app.config import conf, check_test_mode
     from app import app
     from werkzeug.contrib.fixers import ProxyFix
 
-    port = conf['sys']['port']
+    if check_test_mode():
+        port = conf['sys']['port']
+    else:
+        import os
+        port = os.environ['PORT']
     ops = { 'bind': '0.0.0.0:%s' % str(port) }
 
     app.wsgi_app = ProxyFix(app.wsgi_app)
